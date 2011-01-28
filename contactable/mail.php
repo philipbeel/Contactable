607e057d9f198f51091dca875ba424200f743485
philipbeel <contact@theodin.co.uk>
2011-01-28 02:46:01 -0800

    added PHP5's email filter var()

--- contactable/mail.php
+++ contactable/mail.php
@@ -7,7 +7,7 @@
 	$contactMessage = "Message: $comment \r \n From: $name \r \n Reply to: $emailAddr";
 
 	//validate the email address on the server side
-	if(eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $emailAddr) ) {
+	if(filter_var($emailAddr, FILTER_VALIDATE_EMAIL) ) {
 		//if successful lets send the message
 		mail('/*RECIPIENTS EMAIL ADDRESS HERE*/', $subject, $contactMessage);
 		echo('success'); //return success callback
