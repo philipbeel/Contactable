e074362c5a623798bbe1ed093afc5614b910d532
Chuck Houpt <chuck@habilis.net>
2012-07-08 16:22:09 -0400

    Moved all class and id names into a psuedo-namespace by prefixing 'contactable-' This fixes naming conflicts due to use of generic ids like 'name'. See name.html for demo of problems.

--- jquery.contactable.js
+++ jquery.contactable.js
@@ -34,15 +34,14 @@
 		return this.each(function() {
 
 			// Create the form and inject it into the DOM
-			var this_id_prefix = '#'+this.id+' '
-			,	dropdown = ''
+			var dropdown = ''
 			,	filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
 			,	dropdownLen = options.dropdownOptions.length
 			,	i;
 
 			// Add select option if applicable
 			if(options.dropdownTitle) {
-				dropdown += '<p><label for="dropdown">'+options.dropdownTitle+' </label><br /><select name="dropdown" id="dropdown" class="dropdown">';
+				dropdown += '<p><label for="contactable-dropdown">'+options.dropdownTitle+' </label><br /><select name="dropdown" id="contactable-dropdown" class="contactable-dropdown">';
 
 				for(i=0; i < dropdownLen; i++) {
 					dropdown += '<option value="'+options.dropdownOptions[i]+'">'+options.dropdownOptions[i]+'</option>';
@@ -52,71 +51,71 @@
 			}
 			// Form layout
 			/*	
-			*	<div id="contactable_inner"></div>
-			*	<form id="contactForm" method="" action="">
-			*  		<div id="loading"></div>
-			*		<div id="callback"></div>
-			* 		<div class="holder">
+			*	<div id="contactable-inner"></div>
+			*	<form id="contactable-contactForm" method="" action="">
+			*  		<div id="contactable-loading"></div>
+			*		<div id="contactable-callback"></div>
+			* 		<div class="contactable-holder">
 			* 			<p>
-			*				<label for="name">Name<span class="green"> * </span></label><br />
-			*				<input id="name" class="contact validate" name="name" />
+			*				<label for="contactable-name">Name<span class="contactable-green"> * </span></label><br />
+			*				<input id="contactable-name" class="contactable-contact contactable-validate" name="name" />
 			*			</p>
 			*			<p>
-			*				<label for="email"> Email address <span class="green"> * </span></label><br />
-			* 				<input id="email" class="contact validate" name="email" />
+			*				<label for="contactable-email"> Email address <span class="contactable-green"> * </span></label><br />
+			* 				<input id="contactable-email" class="contactable-contact contactable-validate" name="email" />
 			*			</p>
 			* 			<p>
-			*				<label for="message"> Message <span class="green"> * </span></label><br />
-			* 				<textarea id="message" name="message" class="message validate" rows="4" cols="30" ></textarea>
+			*				<label for="contactable-message"> Message <span class="contactable-green"> * </span></label><br />
+			* 				<textarea id="contactable-message" name="message" class="contactable-message contactable-validate" rows="4" cols="30" ></textarea>
 			*			</p>
 			*			<p>
-			*				<input class="submit" type="submit" value="Submit"/>
+			*				<input class="contactable-submit" type="submit" value="Submit"/>
 			*			</p>
-			*			<p class="disclaimer">Disclaimer</p>
+			*			<p class="contactable-disclaimer">Disclaimer</p>
 			*		</div>
 			*	</form>
 			*/
 
-			jQuery(this).html('<div id="contactable_inner"></div><form id="contactForm" method="" action=""><div id="loading"></div><div id="callback"></div><div class="holder"><p><label for="name">'+options.name+'<span class="green"> * </span></label><br /><input id="name" class="contact validate" name="name" /></p><p><label for="email">'+options.email+' <span class="green"> * </span></label><br /><input id="email" class="contact validate" name="email" /></p>'+dropdown+'<p><label for="message">'+options.message+' <span class="green"> * </span></label><br /><textarea id="message" name="message" class="message validate" rows="4" cols="30" ></textarea></p><p><input class="submit" type="submit" value="'+options.submit+'"/></p><p class="disclaimer">'+options.disclaimer+'</p></div></form>');
+			jQuery(this).html('<div id="contactable-inner"></div><form id="contactable-contactForm" method="" action=""><div id="contactable-loading"></div><div id="contactable-callback"></div><div class="contactable-holder"><p><label for="contactable-name">'+options.name+'<span class="contactable-green"> * </span></label><br /><input id="contactable-name" class="contactable-contact contactable-validate" name="name" /></p><p><label for="contactable-email">'+options.email+' <span class="contactable-green"> * </span></label><br /><input id="contactable-email" class="contactable-contact contactable-validate" name="email" /></p>'+dropdown+'<p><label for="contactable-message">'+options.message+' <span class="contactable-green"> * </span></label><br /><textarea id="contactable-message" name="message" class="contactable-message contactable-validate" rows="4" cols="30" ></textarea></p><p><input class="contactable-submit" type="submit" value="'+options.submit+'"/></p><p class="contactable-disclaimer">'+options.disclaimer+'</p></div></form>');
 			
 			// Toggle the form visibility
-			jQuery(this_id_prefix+'div#contactable_inner').toggle(function() {
-				jQuery(this_id_prefix+'#overlay').css({display: 'block'});
+			jQuery('#contactable-inner').toggle(function() {
+				jQuery('#contactable-overlay').css({display: 'block'});
 				jQuery(this).animate({"marginLeft": "-=5px"}, "2000"); 
-				jQuery(this_id_prefix+'#contactForm').animate({"marginLeft": "-=0px"}, "2000");
+				jQuery('#contactable-contactForm').animate({"marginLeft": "-=0px"}, "2000");
 				jQuery(this).animate({"marginLeft": "+=387px"}, "4000"); 
-				jQuery(this_id_prefix+'#contactForm').animate({"marginLeft": "+=390px"}, "4000"); 
+				jQuery('#contactable-contactForm').animate({"marginLeft": "+=390px"}, "4000"); 
 			}, 
 			function() {
-				jQuery(this_id_prefix+'#contactForm').animate({"marginLeft": "-=390px"}, "4000");
+				jQuery('#contactable-contactForm').animate({"marginLeft": "-=390px"}, "4000");
 				jQuery(this).animate({"marginLeft": "-=387px"}, "4000").animate({"marginLeft": "+=5px"}, "2000"); 
-				jQuery(this_id_prefix+'#overlay').css({display: 'none'});
+				jQuery('#contactable-overlay').css({display: 'none'});
 			});
 			
 			// Submit the form
-			jQuery(this_id_prefix+"#contactForm").submit(function() {
+			jQuery("#contactable-contactForm").submit(function() {
 				
 				// Validate the entries
 				var valid = true
 				,	params;
 
 				//Remove any previous errors
-				jQuery(this_id_prefix+"#contactForm .validate").each(function() {
-					jQuery(this).removeClass('invalid');
+				jQuery("#contactable-contactForm .contactable-validate").each(function() {
+					jQuery(this).removeClass('contactable-invalid');
 				});
 
 				// Loop through requigreen field
-				jQuery(this_id_prefix+"#contactForm .validate").each(function() {
+				jQuery("#contactable-contactForm .contactable-validate").each(function() {
 					
 					// Check the min length
 					if(jQuery(this).val().length < 2) {
-						jQuery(this).addClass("invalid");
+						jQuery(this).addClass("contactable-invalid");
 						valid = false;
 					}
 
 					//Check email is valid
-					if (!filter.test(jQuery(this_id_prefix+"#contactForm #email").val())) {
-						jQuery(this_id_prefix+"#contactForm #email").addClass("invalid");
+					if (!filter.test(jQuery("#contactable-contactForm #contactable-email").val())) {
+						jQuery("#contactable-contactForm #contactable-email").addClass("contactable-invalid");
 						valid = false;
 					}						
 				});
@@ -129,8 +128,8 @@
 
 			function submitForm() {
 				// Display loading animation
-				jQuery(this_id_prefix+'.holder').hide();
-				jQuery(this_id_prefix+'#loading').show();
+				jQuery('.contactable-holder').hide();
+				jQuery('#contactable-loading').show();
 				
 				// Trigger form submission if form is valid
 				jQuery.ajax({
@@ -138,35 +137,35 @@
 					url: options.url,
 					data: {
 						subject:options.subject, 
-						name:jQuery(this_id_prefix+'#name').val(), 
-						email:jQuery(this_id_prefix+'#email').val(), 
-						issue:jQuery(this_id_prefix+'#dropdown').val(), 
-						message:jQuery(this_id_prefix+'#message').val()
+						name:jQuery('#contactable-name').val(), 
+						email:jQuery('#contactable-email').val(), 
+						issue:jQuery('#contactable-dropdown').val(), 
+						message:jQuery('#contactable-message').val()
 					},
 					success: function(data) {
 						// Hide loading animation
-						jQuery(this_id_prefix+'#loading').css({display:'none'}); 
+						jQuery('#contactable-loading').css({display:'none'}); 
 
 						// Check for a valid server side response
 						if( data.response === 'success') {
-							jQuery(this_id_prefix+'#callback').show().append(options.recievedMsg);
+							jQuery('#contactable-callback').show().append(options.recievedMsg);
 							if(options.hideOnSubmit === true) {
 								//hide the tab after successful submition if requested
-								jQuery(this_id_prefix+'#contactForm').animate({dummy:1}, 2000).animate({"marginLeft": "-=450px"}, "slow");
-								jQuery(this_id_prefix+'div#contactable_inner').animate({dummy:1}, 2000).animate({"marginLeft": "-=447px"}, "slow").animate({"marginLeft": "+=5px"}, "fast"); 
-								jQuery(this_id_prefix+'#overlay').css({display: 'none'});	
+								jQuery('#contactable-contactForm').animate({dummy:1}, 2000).animate({"marginLeft": "-=450px"}, "slow");
+								jQuery('#contactable-inner').animate({dummy:1}, 2000).animate({"marginLeft": "-=447px"}, "slow").animate({"marginLeft": "+=5px"}, "fast"); 
+								jQuery('#contactable-overlay').css({display: 'none'});	
 							}
 						} else {
-							jQuery(this_id_prefix+'#callback').show().append(options.notRecievedMsg);
+							jQuery('#contactable-callback').show().append(options.notRecievedMsg);
 							setTimeout(function(){
-								jQuery(this_id_prefix+'.holder').show();
-								jQuery(this_id_prefix+'#callback').hide().html('');
+								jQuery('.contactable-holder').show();
+								jQuery('#contactable-callback').hide().html('');
 							},2000);
 						}
 					},
 					error:function(e){
-						jQuery(this_id_prefix+'#loading').css({display:'none'}); 
-						jQuery(this_id_prefix+'#callback').show().append(options.notRecievedMsg);
+						jQuery('#contactable-loading').css({display:'none'}); 
+						jQuery('#contactable-callback').show().append(options.notRecievedMsg);
 					}
 				});		
 			}
